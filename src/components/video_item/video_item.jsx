6d65a70f9701e388119caacfb3f3f1cf7121c3aa
mjkim <91gem@naver.com>
2022-04-22 14:37:12 +0900

    create video items

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => (
-            <h1>{props.video.snippet.title}</h1>
-    );
+const VideoItem = ({video: {snippet}}) => (
+    <li className={styles.container}>
+        <div className={styles.video}>
+            <div className={styles.thumbnail}>
+                <img className={styles.thumbImg} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
+            </div>
+            
+            <div className={styles.textArea}>
+                <p className={styles.title}>{snippet.title}</p>
+                <span className={styles.channel}>{snippet.channelTitle}</span>
+            </div>
+        </div>
+    </li>
+);
 
 export default VideoItem;
