4274cf5e754603ad58e3ee2dfcffeb881bc8c2a3
mjkim <91gem@naver.com>
2022-04-22 16:19:40 +0900

    검색창 & 결과 만들기

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './app.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = keyword => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${keyword}&key=AIzaSyAWtYKYGSdiZEgsTe49lo1eMJKypePBuFk&type=video`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map( item => ({...item, id: item.id.videoId })))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
 
   useEffect(() => {
     const requestOptions = {
@@ -18,7 +31,12 @@ function App() {
       
     }, []);
     
-  return <VideoList videos={videos} />
+  return (
+    <>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,34 @@
+import styles from './search_header.module.css';
+import React, { useRef } from 'react';
+
+const SearchHeader = ({onSearch}) => {
+    const searchRef = useRef()
+    const hadnleSearch = () => {
+        const value = searchRef.current.value;
+        onSearch(value)
+        searchRef.current.value = '';
+    }
+    const onClick = () => {
+        hadnleSearch()
+    }
+    const onKeyPress = (event) => {
+       if( event.key === "Enter" ){
+        hadnleSearch()
+       }
+
+    }
+    
+    return(
+        <header className={styles.header}>
+            <div className={styles.inner}>
+                <h1 className={styles.logo}><img src='/images/logo.png' alt='logo' /></h1>
+                <div className={styles.searchArea}>
+                    <input ref={searchRef} className={styles.header} type="search" placeholder='Enter what you wanna watch' onKeyPress={onKeyPress}  />
+                    <button className={styles.search} type='submit' onClick={onClick}>Search</button>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default SearchHeader;
