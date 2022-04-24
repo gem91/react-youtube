3bd4afe12d3b37b7fcbb329c706838d6384f45f6
mjkim <91gem@naver.com>
2022-04-24 20:25:41 +0900

    iframe 오타수정

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -5,9 +5,9 @@ const VideoDetail = ({video, video: {snippet}}) => {
     console.log(video);
     return (
         <section className={styles.detail}>
-            <iframe className={styles.video} id="ytplayer" type="text/html" width="100%" height="500px" title='test'
+            <iframe className={styles.video} id="ytplayer" title="youtube video title" type="text/html" width="100%" height="500px" title='test'
             src={`https://www.youtube.com/embed/${video.id}`}
-            frameborder="0" allowfullscreen></iframe>
+            frameBorder="0" allowFullScreen></iframe>
             <h2>{snippet.title}</h2>
             <span className={styles.channel}>{snippet.channelTitle}</span>
             <p className={styles.desc}>{snippet.description}</p>
